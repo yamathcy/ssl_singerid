7eadcb08dfeda17e982d7fd5a9e7ee70e2d8d994
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-19 20:19:50 +0900

    chunk data

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ train.py
 """
 
 def train(model, train_loader, valid_loader, logger, max_epochs=100, fp=True):
-    precision = "16-mixed" if fp else None
+    precision = "16-mixed" if fp else 32
     trainer = pl.Trainer(max_epochs=max_epochs, deterministic=True, check_val_every_n_epoch=10, logger=logger, precision=precision)
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
     return model, trainer
