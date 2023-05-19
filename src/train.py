c3f26f5565c65220de60288a98c5df1e9dbc8cfe
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-19 18:04:33 +0900

    chunk data

--- src/train.py
+++ src/train.py
@@ -7,7 +7,8 @@ train.py
 モデルの学習に関する処理をここに書く
 """
 
-def train(model, train_loader, valid_loader, logger, max_epochs=100):
-    trainer = pl.Trainer(max_epochs=max_epochs, deterministic=True, check_val_every_n_epoch=10, logger=logger)
+def train(model, train_loader, valid_loader, logger, max_epochs=100, fp=True):
+    precision = "16-mixed" if fp else None
+    trainer = pl.Trainer(max_epochs=max_epochs, deterministic=True, check_val_every_n_epoch=10, logger=logger, precision=precision)
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
     return model, trainer
