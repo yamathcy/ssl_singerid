fb7cbe3df93be30bb216c66297563ab69e1994a3
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-20 02:04:16 +0900

    remove mlflow

--- src/model.py
+++ src/model.py
@@ -270,7 +270,7 @@ class SSLNet(BaseModel):
     def forward(self, x):
         # print(x.shape)
         # x = x.squeeze(dim=1)
-        print(x.shape)
+        print(x.shape, type(x))
         # x = x.to(DEVICE) # FIXME: Unknown behaviour on return to cpu by feature extractor
         x = self.frontend(x, output_hidden_states=True, return_dict=None, output_attentions=None)
         h = x["hidden_states"]
