4648ef1885604d75c6f2f5f854c8f326f52dfcbf
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-19 12:51:49 +0900

    typo

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def main(conf:omegaconf.DictConfig):
     # モデル
     # パラメータによって条件を分岐
     if conf.model == "crnn":
-        model = AudioDNN(conf, num_classes=20)
+        model = CRNN(conf.sr, num_classes=20)
     elif conf.model == "ssl":
         model = SSLNet(conf,weights=None,url=conf.url,class_num=20)
     else:
