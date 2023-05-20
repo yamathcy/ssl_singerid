2560a6ecd668749cb970d74a332d7aee4eb513f5
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-20 19:52:29 +0900

    remove mlflow

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ import wandb
 
 """
 main.py
-メインの根幹部分．ここにロジックをまとめる．
 """
 
 '''+++'''
@@ -33,7 +32,7 @@ def main(conf:omegaconf.DictConfig):
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     # torch.set_float32_matmul_precision('high')
 
-    # ランダムのシードを決定
+    # seed
     SEED = 42
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
@@ -42,14 +41,13 @@ def main(conf:omegaconf.DictConfig):
     np.random.seed(SEED)
 
 
-    # パラメータのロギング
-    # wandbの準備
+    # logging
+    # wandb
     # wandb.init(config=conf)
     logger = WandbLogger(name=conf.experiment_name, project="Singer Identification")
     logger.log_hyperparams(conf)
 
     '''+++'''
-    # トラッキングを行う場所をチェックし，ログを収納するディレクトリを指定
     # print(hydra.utils.get_original_cwd())
     # dir = hydra.utils.get_original_cwd() + "/mlruns"
     # if not os.path.exists(dir):
@@ -57,27 +55,27 @@ def main(conf:omegaconf.DictConfig):
 
     '''+++'''
     
-    # mlflowの準備
+    # mlflow
     # mlflow.set_tracking_uri(dir)
     # tracking_uri = mlflow.get_tracking_uri()
     # mlflow.set_experiment(conf.experiment_name)
 
-    # GPUの準備
+    # GPU
     use_cuda = torch.cuda.is_available()
     Device = torch.device("cuda" if use_cuda else "cpu")
     print(Device)
 
-    # 学習したモデルのパラメータ
+    # parameters
     out_model_fn = './model/%s' % (conf.savename)
     if not os.path.exists(out_model_fn):
         os.makedirs(out_model_fn)
 
-    # データセットの読み込み
+    # load dataset
     data_path = conf.data_path
     # meta_path = os.path.join(hydra.utils.get_original_cwd(), data_path,'meta/esc50.csv')
     # df = pd.read_csv(meta_path)
 
-    # 音ファイルの読み込み
+    # soundfile
     audio_path = os.path.join(hydra.utils.get_original_cwd(), data_path)
     print("load audio... path: {}".format(audio_path))
     train_data = Artist(audio_path, sr=conf.sr, chunk_length=conf.length, set=[0,1,2,3])
@@ -87,16 +85,16 @@ def main(conf:omegaconf.DictConfig):
     test_data = Artist(audio_path, sr=conf.sr, chunk_length=conf.length, set=[5])
     print("chunked audio: train: {}, valid: {}, test: {}".format(len(train_data), len(valid_data), len(test_data)))
 
-    # classidを得る
+    # classid
     target_class = train_data.get_class_to_id()
 
-    # 各データローダーの用意
+    # dataloader
     train_loader = DataLoader(train_data, batch_size=conf.batch_size, shuffle=True)
     valid_loader = DataLoader(valid_data, batch_size=16, shuffle=False)
     test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
 
-    # モデル
-    # パラメータによって条件を分岐
+    # model
+    # parameter
     if conf.model == "crnn":
         model = CRNN(conf, classes_num=20)
     elif conf.model == "ssl":
@@ -112,10 +110,10 @@ def main(conf:omegaconf.DictConfig):
     # out,_ = model(test_input)
 
     '''+++'''
-    # 学習
+    # train
     model, trainer = train(model, train_loader=train_loader, valid_loader=valid_loader, max_epochs=conf.epoch, logger=logger, fp=conf.fp)
 
-    # 評価
+    # evaluation
     evaluation(model, logger, test_loader, target_class)
 
     '''+++'''
