de53246b3e04126deca328bad443a731188665e3
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-22 18:09:22 +0900

    s

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ main.py
 '''+++'''
 @hydra.main(config_name="config")
 def main(conf):
-    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    # torch.set_float32_matmul_precision('high')
+    # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    # # torch.set_float32_matmul_precision('high')
 
     # seed
     SEED = 42
