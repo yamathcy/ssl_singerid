66f743fd570b8e4a9d0bf2ebfc7b9ce4e1a6e13e
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-05-24 17:43:20 +0900

    updata r.txt

--- src/dataio.py
+++ src/dataio.py
@@ -119,7 +119,7 @@ class Artist_from_numpy(torch.utils.data.Dataset):
         print("load audio from", dir)
         for fold in set:
             folddata = sorted(glob.glob(os.path.join(dir, "*-{}-*-*.npy".format(fold))))
-            print("data sample:{}".len(folddata))
+            print("data sample:{}".format(len(folddata)))
             # print('fold data: {}'.format(folddata))
             self.data.extend(folddata)
         for data in self.data:
